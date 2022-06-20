465fed005349c9a6ea39a5385e95b5c95a400cca
kudic <Kudickadifa@gmail.com>
2022-06-19 21:27:35 -0500

    I added example class

---
+++ src/day14_selfPractice/SumOfAllNumbers.java
@@ -0,0 +1,37 @@
+package day14_selfPractice;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class SumOfAllNumbers {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        int num = new Scanner(System.in).nextInt();
+
+        if (num < 0) {
+            System.out.println("Invalid number");
+        }else{
+            int sum = 0;
+            for (int i = 0; i <= num ; i++) {
+                sum += i;
+                
+            }
+            System.out.println(sum);
+
+        }
+
+
+
+    }
+}
+/*
+write a program
+that can calculate the sum of all numbers between 1 to any given number
+
+        ex:
+        input: 100
+        output: 5050
+        input: 50
+         output: 1275
+
+ */
