7232b9bb7c3e8f0a5184dc8f68d1e65ac263f1d3
kudic <Kudickadifa@gmail.com>
2022-06-24 20:40:22 -0500

    I added example class

--- src/day28_selfPractice/pizza/TestPizzaObject.java
+++ src/day28_selfPractice/pizza/TestPizzaObject.java
@@ -3,11 +3,13 @@ package day28_selfPractice.pizza;
 public class TestPizzaObject {
     public static void main(String[] args) {
 
-        Pizza pizza1 = new Pizza("Small",1, 2);
-        Pizza pizza2 = new Pizza("Medium", 2,2);
-        Pizza pizza3 = new Pizza("Large", 1,1);
+        Pizza pizza1 = new Pizza("Small",3, 2);
 
-        System.out.println("pizza1=" + pizza1 + "\n" +"pizza2=" + pizza2+ "\n" +"pizza3 =" + pizza3 );
+       pizza1.getSize();
+       pizza1.getNumberOfCheeseTopping();
+       pizza1.getNumberOfPepperoniTopping();
+       pizza1.calCost();
+        System.out.println(pizza1);
 
 
     }
