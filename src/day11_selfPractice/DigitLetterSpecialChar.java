52822eaa2c522dbe9558e323f3bfdbe1952bdbe3
kudic <Kudickadifa@gmail.com>
2022-06-27 22:27:47 -0500

    I added example class

--- src/day11_selfPractice/DigitLetterSpecialChar.java
+++ src/day11_selfPractice/DigitLetterSpecialChar.java
@@ -1,27 +1,43 @@
 package day11_selfPractice;
 
+import day26_selfPractice.CydeoStudent;
+
 import java.util.Scanner;
 
 public class DigitLetterSpecialChar {
 
     public static void main(String[] args) {
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter a word");
-        String word = scan.nextLine();
 
-        char firstChar = word.charAt(0);
 
-        if (firstChar >= 97 && firstChar <= 98) {
-            System.out.println("First character is Digit");
-        }else if(firstChar>=99 && firstChar<=100) {
-            System.out.println("first character is lowercase letter");
-        }else if(firstChar >= 101 && firstChar<= 102){
-            System.out.println("first character is uppercase letter");
-        }else if (firstChar >=50 && firstChar<54){
-            System.out.println("first character is special character");
+        String str = new Scanner(System.in).nextLine();//for scanner to use only one time /no need to closed scanner
+
+        if (str.length() >= 1) {//if the string has at least one character//nested if statement
+
+            char f = str.charAt(0);//first character of string
+
+            if (f >= '0' && f <= 9) {
+
+                System.out.println("First character is Digit");
+            } else if (f >= 'A' && f <= 'Z') {
+                System.out.println("first character is uppercase letter");
+            } else if (f >= 'a' && f <= 'z') {
+                System.out.println("first character is lowercase letter");
+            } else {
+                System.out.println("first character is special character");
+            }
+        }else{//if string is empty
+            System.out.println("String is Empty");
         }
+
+
+
+
+
+
     }
+
+
 }
 /*
 3. Create a class named DigitLetterSpecialChar and write a program that can ask the user to enter a word
