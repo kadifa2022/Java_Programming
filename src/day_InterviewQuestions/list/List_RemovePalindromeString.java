15f228034cdcaad716f9e184ae30138cb6f80b84
kudic <Kudickadifa@gmail.com>
2022-07-23 16:14:40 -0500

    Practice

---
+++ src/day_InterviewQuestions/list/List_RemovePalindromeString.java
@@ -0,0 +1,45 @@
+package day_InterviewQuestions.list;
+
+import javax.print.DocFlavor;
+import java.util.Iterator;
+import java.util.List;
+
+public class List_RemovePalindromeString {
+
+    /*
+    remove palindrome strings from a List of String
+    */
+    //Solution1
+    public static List<String> removePalindrome1(List<String> list){
+        Iterator<String> it = list.iterator();
+        while (it.hasNext()){
+            String word = it.next();
+            String reversed= "";
+            for (int i = 0; i < word.length() -1; i--) {
+                reversed += word.charAt(i);
+            }
+            if(word.equalsIgnoreCase(reversed)){
+                it.remove();;
+            }
+
+        }
+        return list;
+    }
+
+    //Solution2
+
+    public List<String> removePalindrome2(List<String>list){
+        list.removeIf(p-> isPalindrome(p));
+        return list;
+    }
+    public static boolean isPalindrome (String str){
+        String reversed = "";
+        for (int i = 0; i < str.length() -1; i--) {
+            reversed +=str.charAt(i);
+
+
+        }
+
+        return reversed.equalsIgnoreCase(str);
+    }
+}
