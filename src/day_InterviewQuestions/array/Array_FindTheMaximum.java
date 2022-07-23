0f0bd6fd190636b0304f71393dc68c17d1bc4de9
kudic <Kudickadifa@gmail.com>
2022-07-23 18:55:16 -0500

    Practice

---
+++ src/day_InterviewQuestions/array/Array_FindTheMaximum.java
@@ -0,0 +1,19 @@
+package day_InterviewQuestions.array;
+
+public class Array_FindTheMaximum {
+        /*
+    Write a function that can find the maximum number from an int Array
+     */
+
+    public static int maxValue(int[] n) {
+        int max = Integer.MAX_VALUE;
+        for (int each: n) {
+            if (each > max){
+                max = each;
+            }
+
+        }
+
+        return max;
+    }
+}
