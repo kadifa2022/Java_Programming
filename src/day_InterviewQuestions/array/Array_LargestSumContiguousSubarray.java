4201dad7de87c8b8618b44a062e62ee4ff111253
kudic <Kudickadifa@gmail.com>
2022-07-24 07:58:39 -0500

    Practice

---
+++ src/day_InterviewQuestions/array/Array_LargestSumContiguousSubarray.java
@@ -0,0 +1,22 @@
+package day_InterviewQuestions.array;
+
+public class Array_LargestSumContiguousSubarray {
+        /*
+    Largest Sum Contiguous Subarray
+    Write a program to find the sum of contiguous subarray within a one-dimensional array of numbers that has the largest sum.
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: [4,-1,2,1] has the largest sum = 6.
+    */
+
+        public static int maxSum(int[] arr){
+            int max =  0;
+            int temp= 0;
+            for (int each: arr) {
+                temp = temp + each;
+                temp = Integer.max(max, 0);
+                max = Integer.max(max, temp);
+            }
+            return max;
+        }
+}
