56c9a4b40ac0c9f7aed407b75771f40a606297e9
kudic <Kudickadifa@gmail.com>
2022-07-25 11:19:41 -0500

    Practice

--- src/practice/mentorsInterviewQuestions/LeapYear.java
+++ src/practice/mentorsInterviewQuestions/LeapYear.java
@@ -10,7 +10,7 @@ public class LeapYear {
         Scanner sc= new Scanner(System.in);
         year = sc.nextInt();
 
-
+        //evenly divisible (%4==0) logical And (%400=0) logical And(%100 not equal 0)
          if(((year % 4 == 0) && (year % 100!=0))|| (year % 400==0)) {
 
              System.out.println("Specified year is a leap year");
