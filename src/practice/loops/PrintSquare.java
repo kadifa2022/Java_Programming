d926afb70fcbf7e605a1220dc6b63cd8c14e3362
kudic <Kudickadifa@gmail.com>
2022-07-26 18:08:15 -0500

    Practice

--- src/practice/loops/PrintSquare.java
+++ src/practice/loops/PrintSquare.java
@@ -8,7 +8,7 @@ public class PrintSquare {
 
     public static void main(String[] args) {
 
-                for (int i = 0; i <= 15; i++) {
+               for (int i = 0; i <= 15; i++) {
                 for (int j = 0; j <= 20; j++) {
                     System.out.print("* ");
 
@@ -17,4 +17,10 @@ public class PrintSquare {
 
                 }
         }
-}
+
+
+
+
+        }
+
+
---
+++ src/practice/loops/TriAngle.java
@@ -0,0 +1,68 @@
+package practice.loops;
+
+public class TriAngle {
+
+    public static void main(String[] args) {
+
+        for (int i = 1 ; i <= 5; i++) {
+            for (int j = 1;  j <= i; j++) {
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+        System.out.println("------------------------------------------------------------------");
+
+        for (int i = 1; i <=5; i++){
+            for (int j = 1; j <= i; j++) {
+                System.out.print("@ ");
+
+            }
+            System.out.println();
+        }
+        System.out.println("---------------------------------------------------------------------");
+
+        for (int i = 1; i <=6 ; i++) {
+            for (int j = 1; j <=6-i ; j++) {
+                System.out.print("* ");
+
+            }
+            System.out.println();
+
+        }
+        System.out.println("------------------------------------------------------------------------");
+       //repeating  same number
+        for (int i = 1; i < 6; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(i+ "");
+
+            }
+            System.out.println();
+
+        }
+        System.out.println("---------------------------------------------------------------------");
+                // printing in ascending order
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print(j+"");
+
+            }
+            System.out.println();
+
+        }
+        System.out.println("-------------------------------------------------------------------");
+
+        for (int i = 1; i <= 6 ; i++) {
+            for (int j = 6; j >= i; j--) {
+                System.out.print(j+"");
+
+            }
+            System.out.println();
+        }
+
+
+    }
+
+}
+
+
