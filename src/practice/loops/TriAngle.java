5517170f3c216d44191721cd269fe4851405509f
kudic <Kudickadifa@gmail.com>
2022-07-26 18:18:26 -0500

    Practice

--- src/practice/loops/TriAngle.java
+++ src/practice/loops/TriAngle.java
@@ -59,6 +59,19 @@ public class TriAngle {
             }
             System.out.println();
         }
+        System.out.println("----------------------------------------------------------------------");
+
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <i ; j++) {
+                System.out.print("* ");
+            }
+            for (int k = 6-i; k>=1; k--) {
+                System.out.print(k+ "");
+                
+            }
+            System.out.println();
+            
+        }
 
 
     }
