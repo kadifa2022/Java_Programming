c67ca15cdc0954244db0af5371ae3dcb9675adb2
kudic <Kudickadifa@gmail.com>
2022-07-26 05:24:34 -0500

    Practice

--- src/practice/mentorsInterviewQuestions/FindTheFirstAndLast.java
+++ src/practice/mentorsInterviewQuestions/FindTheFirstAndLast.java
@@ -1,6 +1,10 @@
 package practice.mentorsInterviewQuestions;
 
-public class FindTheFirstAndLast{
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.Arrays;
+
+public class FindTheFirstAndLast {
 
     /*
     Write a function (getSmallestAndLargest) that finds the lexicographically
@@ -9,15 +13,30 @@ public class FindTheFirstAndLast{
 
      */
 
-    static String finedFirstAndLast(String s, int k){
-        int a = s.length()-k + 1;
+    static String finedFirstAndLast(String s, int k) {
+        int a = s.length() - k + 1;
         String[] str = new String[a];
-        for (int i = 0; i < a; i++){
-            str[i] = s.substring(i, i+k);
+        for (int i = 0; i < a; i++) {
+            str[i] = s.substring(i, i + k);
 
         }
         return finedFirstAndLast(s, k);
 
 
     }
+    static String finedFirstAndLast2(String s, int k){
+        int a = s.length() - k +1;
+        String []str = new String[a];
+        for (int i = 0; i < a; i++) {
+            str[i] = s.substring(i,i +k);
+
+            System.out.println(Arrays.toString(str));
+            Arrays.sort(str);
+            System.out.println(Arrays.toString(str));
+
+            
+        }
+        return str [0]+"\n"+str[a-1];
+    }
+
 }
