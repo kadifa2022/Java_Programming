0eee3e2fba32dc730a1bd3fdc9ba97da5decb97e
kudic <Kudickadifa@gmail.com>
2022-07-25 21:35:59 -0500

    Practice

---
+++ src/practice/mentorsInterviewQuestions/TreeSet_FindFirsAndLast.java
@@ -0,0 +1,29 @@
+package practice.mentorsInterviewQuestions;
+
+import java.util.TreeSet;
+
+public class TreeSet_FindFirsAndLast {
+
+    static String findFirsAndLast(String s, int k){
+        int a = s.length()-k +1;
+
+        TreeSet<String> set = new TreeSet<>();
+        for (int i = 0; i < a; i++) {
+            set.add(s.substring(i, i + k));
+
+            
+        }
+        System.out.println(set);
+        return set.first()+"\n"+set.last();
+
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+
+
+}
