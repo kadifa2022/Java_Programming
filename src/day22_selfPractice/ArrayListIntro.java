0ad9903b63f24118b5ade03a9175b3df96508f34
kudic <Kudickadifa@gmail.com>
2022-07-28 09:05:44 -0500

    Practice

---
+++ src/day22_selfPractice/ArrayListIntro.java
@@ -0,0 +1,20 @@
+package day22_selfPractice;
+
+import java.util.ArrayList;
+
+public class ArrayListIntro {
+
+    public static void main(String[] args) {
+                                //calling constructor
+        ArrayList<Integer>list = new ArrayList<>();
+
+        list.add(10);
+        list.add(10);
+        list.add(10);
+
+
+        System.out.println(list);
+        System.out.println(list.size());
+    }
+
+}
