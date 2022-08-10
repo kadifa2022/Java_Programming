e7e3bfea09e7824def8839db69549f9532e76c73
Kadifa <Kudickadifa@gmail.com>
2022-08-10 07:52:04 -0500

    Practice

--- src/day_InterviewQuestions/list/List_RemovePalindromeString1.java
+++ src/day_InterviewQuestions/list/List_RemovePalindromeString1.java
@@ -1,8 +1,10 @@
 package day_InterviewQuestions.list;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import static day_InterviewQuestions.list.List_RemovePalindromeString.isPalindrome;
 
 
 public class List_RemovePalindromeString1 {
@@ -12,21 +14,38 @@ public class List_RemovePalindromeString1 {
     */
     //solution 1
 
-    public static List<String> removePalindrome(List<String>list){
-        Iterator <String> it= list.iterator();
+    public static List<String> removePalindrome(List<String> list) {
+        Iterator<String> it = list.iterator();
         while (it.hasNext()) {
             String word = it.next();
             String reversed = "";
 
 
-            for (int i = 0; i <word.length()-1 ; i--) {
+            for (int i = 0; i < word.length() - 1; i--) {
                 reversed += word.charAt(i);
             }
-            if (word.equalsIgnoreCase(reversed)){
+            if (word.equalsIgnoreCase(reversed)) {
                 it.remove();
             }
         }
         return list;
+    }
+
+    //Solution2
+    public static List<String> removePalindrome1(List<String> list) {
+        list.removeIf(p -> isPalindrome(p));
+        return list;
+    }
+
+    public static boolean isPalindrome(String str) {
+        String reversed = "";
+
+
+        for (int i = 0; i < str.length() - 1; i--) {
+            reversed += str.charAt(i);
+
+        }
+        return reversed.equalsIgnoreCase(str);
     }
 
 }
