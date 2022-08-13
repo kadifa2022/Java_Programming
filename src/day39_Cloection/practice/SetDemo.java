d7384d38059e530163bcbf4fc9ce5e14dcce45fc
Kadifa <Kudickadifa@gmail.com>
2022-08-13 15:43:57 -0500

    Practice

--- src/day39_Cloection/practice/SetDemo.java
+++ src/day39_Cloection/practice/SetDemo.java
@@ -50,6 +50,39 @@ public class SetDemo {
 
         System.out.println(set3);
 
+        String [] words={"Java", "Java", "Phayton", "Phyton", "C#", "C#", "Ruby"};
+
+
+        Set<String> results= new LinkedHashSet<>();
+
+        results.addAll(Arrays.asList(words));
+
+
+        System.out.println(results);
+
+       // System.out.println(results.get(1)); no index
+                            //called construction to convert -get all elements -call get() to get index
+        System.out.println(new ArrayList<>(results).get(1));
+
+        words=results.toArray(new String[0]);//toArray()--Array is the fastest collection type
+        System.out.println("Words Array: " + Arrays.toString(words));//converted to array using toArray()
+
+        System.out.println("----------------------------------------------------------");
+
+        List<Integer> numbers = new ArrayList<>();
+
+        numbers.addAll(Arrays.asList(10, 10,10,20,20,20,30,30,30,40,40,40,50,50,50,5,5,5,5,5,1,1,1,1));
+
+        Set<Integer> n = new TreeSet<>(numbers);//ascending order 1,2,3..  no duplicates
+        System.out.println(n);
+
+
+
+
+
+
+
+
 
     }
 }
