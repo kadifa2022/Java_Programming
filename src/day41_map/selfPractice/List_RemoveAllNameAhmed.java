8e0f6e405da6fb0b65b22e254ed01ed7a52d9129
Kadifa <Kudickadifa@gmail.com>
2022-11-18 18:54:48 -0600

    practice

--- src/day41_map/selfPractice/List_RemoveAllNameAhmed.java
+++ src/day41_map/selfPractice/List_RemoveAllNameAhmed.java
@@ -1,10 +1,21 @@
 package day41_map.selfPractice;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
 public class List_RemoveAllNameAhmed {
+
+    public static void main(String[] args) {
+        List<String> names= new ArrayList<>();
+        names.add("Ahmed");
+        names.add("Tony");
+        names.add("Olga");
+        System.out.println(remove3(names ));
+
+
+    }
     /*
     //Given a list of people' names: "Ahmed", "John", Eric", "Ahmed".....
     //Write a java operation to remove all the names named Ahmed
