eab3e4f32a3f377c026ed3e03447d2279c6c6bec
Kadifa <Kudickadifa@gmail.com>
2023-01-19 10:38:50 -0600

    practice

--- src/day41_map/selfPractice/List_MoveAllZeroToTheEnd.java
+++ src/day41_map/selfPractice/List_MoveAllZeroToTheEnd.java
@@ -1,10 +1,24 @@
 package day41_map.selfPractice;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class List_MoveAllZeroToTheEnd {
+    public static void main(String[] args) {
+
+        List<Integer> list= new ArrayList<>();
+        list.add(12);
+        list.add(0);
+        list.add(23);
+        list.add(0);
+        list.add(5);
+
+
+        System.out.println(moveZeroToTheEnd(list));
+
+    }
 
     //write a program that can move all the zero to the end of the List.
 
