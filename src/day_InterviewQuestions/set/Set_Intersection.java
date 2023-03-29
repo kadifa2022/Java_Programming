3b1f87465fd20cb986cfbe2e5bcf7e0a49eacdd8
Kadifa <Kudickadifa@gmail.com>
2023-03-28 22:17:37 -0500

    practice

--- src/day_InterviewQuestions/set/Set_Intersection.java
+++ src/day_InterviewQuestions/set/Set_Intersection.java
@@ -3,15 +3,8 @@ package day_InterviewQuestions.set;
 import java.util.Set;
 
 public class Set_Intersection {
-    /*
-    return a Set equal to the intersection of the parameter Sets
-    */
-    public static Set<Object> getIntersection(Set<Object> a, Set<Object> b) {
 
-        /*
-    Please implement this method to
-    return a Set equal to the intersection of the parameter Sets The method should not chage the content of the parameters.
-    */
+    public static Set<Object> getIntersection(Set<Object> a, Set<Object> b) {
 
         if (a.size()>b.size()){
             a.retainAll(b);
@@ -23,3 +16,8 @@ public class Set_Intersection {
     }
 
 }
+
+        /*
+    Please implement this method to
+    return a Set equal to the intersection of the parameter Sets The method should not chage the content of the parameters.
+    */
