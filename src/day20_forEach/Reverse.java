13936b191a8ac59223c70df4ad26ca8ce47e5cde
Kadifa <Kudickadifa@gmail.com>
2023-03-28 22:17:45 -0500

    practice

--- src/day20_forEach/Reverse.java
+++ src/day20_forEach/Reverse.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class Reverse {
 
     public static void main(String[] args) {
-        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16}; int[] reverse = new int[array.length]; // {5, 4, 3, 2, 1}
+        int[] array = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16};
+
+        int[] reverse = new int[array.length]; // {5, 4, 3, 2, 1}
         for (int i = array.length - 1, j=0; i >= 0; i--) { reverse[j++]= array[i];
         }
         System.out.println(Arrays.toString(reverse));
